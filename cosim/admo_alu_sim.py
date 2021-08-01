5269f759c4970c5869b0cbd30501937babdef096
gustavoale <gustavo.ale.primo@gmail.com>
2021-07-31 23:58:16 -0300

    ALU: Finished all opcodes

--- cosim/admo_alu_sim.py
+++ cosim/admo_alu_sim.py
@@ -28,6 +28,7 @@ import random
 import cocotb
 from cocotb.triggers import Timer
 from admo_alu_model import admo_alu_model
+from admo_defs import *
 
 @cocotb.test()
 async def admo_alu_test(mod):
@@ -38,6 +39,46 @@ async def admo_alu_test(mod):
         val_b = random.randint(0,1<<32)
         val_op = random.randint(0,16) & 0b1111
         # val_op = 0
+        val_res = admo_alu_model(val_a,val_b,val_op)
+
+        mod.operand_a_i <= val_a 
+        mod.operand_b_i <= val_b
+        mod.operator_i <= val_op
+
+        await Timer(1, units='ns')
+        
+        assert mod.result_o == val_res, \
+        f"[{i}] - incorrect for A: {hex(val_a)}, B: {hex(val_b)}, OP: {hex(val_op)} \n"\
+        f"Expected {hex(val_res)}, returned: {hex(mod.result_o.value+0)}"
+
+@cocotb.test()
+async def admo_alu_lts(mod):
+    """ Test admo_alu less than signed"""
+    
+    val_op = ALU_LTS
+    for i in range(10000):
+        val_a = random.randint(0,1<<32)
+        val_b = random.randint(0,1<<32)
+        val_res = admo_alu_model(val_a,val_b,val_op)
+
+        mod.operand_a_i <= val_a 
+        mod.operand_b_i <= val_b
+        mod.operator_i <= val_op
+
+        await Timer(1, units='ns')
+        
+        assert mod.result_o == val_res, \
+        f"[{i}] - incorrect for A: {hex(val_a)}, B: {hex(val_b)}, OP: {hex(val_op)} \n"\
+        f"Expected {hex(val_res)}, returned: {hex(mod.result_o.value+0)}"
+
+@cocotb.test()
+async def admo_alu_ltu(mod):
+    """ Test admo_alu less than unsigned"""
+    
+    val_op = ALU_LTU
+    for i in range(10000):
+        val_a = random.randint(0,1<<32)
+        val_b = random.randint(0,1<<32)
         val_res = admo_alu_model(val_a,val_b,val_op)
 
         mod.operand_a_i <= val_a 
